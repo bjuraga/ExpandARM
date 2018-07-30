3b6e230187b965c4331a684e931f8148e0428e88
Boris Juraga <boris.juraga@live.com>
2018-07-30 23:24:12 +0200

    initial commit

---
+++ src/MergeARM/MergeARM.ps1
@@ -0,0 +1,23 @@
+﻿Param (
+	[string]
+	$fileToExpand
+)
+
+function Main(){
+	# load file to expand
+	$fileContent = LoadARM -file $fileToExpand
+
+	# expand file:// links
+	$expandedFile = ExpandFile $fileContent
+	# store file to originalName.expanded.json
+}
+
+function LoadARM($file){
+	return ( "{" + (Get-Content $file) + "}") | ConvertFrom-Json
+}
+
+function ExpandFile($fileContent){
+	return $fileContent
+}
+
+Main
---
+++ tests/Merge.ARM.Tests/MergeARM.Tests.ps1
@@ -0,0 +1,14 @@
+﻿New-Fixture deploy Clean
+
+$here = (Join-Path -Path (Split-Path (Split-Path (Split-Path -Parent $MyInvocation.MyCommand.Path))) -ChildPath '\src\MergeARM')
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$script = "$here\$sut"
+. "$script"
+
+Describe "LoadFile" {
+    It "loads the file" {
+		$fileContent = LoadARM -file '.\tests\Merge.ARM.Tests\sampleTemplate\Template.json'
+
+		$fileContent.resources[0].apiVersion | Should BeExactly '2017-05-10'
+    }
+}
